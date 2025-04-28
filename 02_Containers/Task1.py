c615172e8aa3529b661eb5863e5f80d6d42d4df8
Youssef19A2 <youssefahmed36567@gmail.com>
2025-04-28 14:56:18 +0300

    Add files via upload

---
+++ 02_Containers/Task1.py
@@ -0,0 +1,12 @@
+'''
+1-Make your module that contain favourite websites and have function 
+called Firefox take url and open website 
+2- then make main file and print menu of sites for user and let him choice
+'''
+
+import Task1_modules
+
+
+Task1_modules.firefox(Task1_modules.facebook_link)
+while True:
+    pass
