89a891202a2d7d958a610ff529422791cd8ddb66
Youssef19A2 <youssefahmed36567@gmail.com>
2025-04-28 19:22:43 +0300

    Add files via upload

---
+++ 03_Advanced/task2.py
@@ -0,0 +1,25 @@
+import pyautogui
+from time import sleep
+import webbrowser
+
+#open gmail inbox
+url="https://mail.google.com/mail/u/0/?hl=ar#inbox"
+webbrowser.open(url)
+sleep(5)
+
+try:
+    #to selct all 
+    pyautogui.moveTo(x=284,y=228,duration=0.5)
+    sleep(2)        
+    pyautogui.click()
+    sleep(2)
+    #to mark as read
+    pyautogui.moveTo(x=492,y=231,duration=0.5)
+    sleep(2)
+    pyautogui.click()
+    #to delete all
+    pyautogui.moveTo(x=433,y=228,duration=0.5)
+    sleep(2)
+    pyautogui.click()
+except :
+    print("not fount")
